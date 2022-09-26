16df729697449220ffa90404ea87bd6d6adf56bc
MirNO89 <108218701+MirNO89@users.noreply.github.com>
2022-09-26 20:29:18 +0200

    Added "old" code to fix earlier errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import Weather from "./Weather";
+
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Oslo" />
+        <footer>
+          <a
+            href="https://github.com/MirNO89/weather-app-shecodes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Miriam MK
+        </footer>
+      </div>
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import WeatherInfo from "./WeatherInfo";
+
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function search() {
+    const apiKey = "a2baf84f083b88654c8b1aba441c30ca";
+    let unit = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+  } else {
+    search();
+    return "The app is loading...";
+  }
+}
